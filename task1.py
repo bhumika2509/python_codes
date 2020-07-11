4bc46215cd9a96af23fe6d0b13d1a55eb7780dda
bhumika2509 <68149153+bhumika2509@users.noreply.github.com>
2020-07-11 16:23:25 +0530

    created python_codes

---
+++ task1.py
@@ -0,0 +1,4 @@
+a=float(input("enter the radius of the circle:"))
+pi=3.1415926536
+area=pi*a*a
+print("the area of the circle with radius "+str(a)+" is:" ,area)
---
+++ task2.py
@@ -0,0 +1,3 @@
+filename=input("Input the filename:")
+f=filename.split(".")
+print("Extension of the file is:" + f[-1])
